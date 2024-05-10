175c7cb968c39af746029c12666b4a8c85d9fba6
sayemhossin <sayemhossin22@gmail.com>
2024-05-10 23:52:44 +0600

    added allquery section

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha1geqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+const database = client.db('ProAlter')
+const allQueryCollection = database.collection('allquery')
+
+
+
+app.post('/allquery', async(req,res)=>{
+    const query = req.body
+    const result = await allQueryCollection.insertOne(query)
+    res.send(result)
+})
+
+app.get('/allquery', async(req,res)=>{
+    const cursor = allQueryCollection.find()
+    const result = await cursor.toArray()
+    res.send(result)
+})
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 
 
 
