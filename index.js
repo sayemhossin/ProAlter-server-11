daff674e466296928f24f2ea367eab5a03df1860
sayemhossin <sayemhossin22@gmail.com>
2024-05-11 03:37:01 +0600

    added specifiq id find

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha1geqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,18 @@ app.get('/allquery', async(req,res)=>{
     const result = await cursor.toArray()
     res.send(result)
 })
+app.get('/allquery/:id', async(req,res) => {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result =  await allQueryCollection.findOne(query)
+    res.send(result)
+})
+
+
+
+
+
+
 // sort
 
 app.get("/myquery/:email", async (req, res) => {
